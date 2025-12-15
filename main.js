0261d6a6fc0d3f3e8898d31c2953c8a03ec847ca
Anita <anitaeli@gmail.com>
2025-12-15 13:41:01 -0300

    funcion principal ok, solo js

--- main.js
+++ main.js
@@ -1,41 +1,67 @@
 
-const nombreUsuario = "David ";
+
+//constantes que por ser una aplicacion con usuario ya ingresado no cambian
+//los variables son segun el dia
+
+const nombreUsuario = "David! ";
 const fechaHoy = new Date();
 let totalPendientesHoy = 3;
 let tieneEventoTarde = true;
 
+//Array
 
-function saludoprincipal(usuario, fecha, pendientes, evento, comprasArray) {
-    let mensajeSaludo = ("Bienvenido "+usuario);
-
-    const listaDeCompras = [
-    "fruta",
-    "verdura",
-    "arroz",
-    "leche",
-    "birra",
-    "pan",
-    "cereales",
-    "fideos",
-    "manteca", 
-    "harina",
-    "azucar",
+const listaDeCompras = [
+   "verdura",
+   "cereales",
+   "shampoo",
+   "huevos",
+   "birra",
+   "jamon",
+   "leche",
+   "dulce de leche",
+   "pan"
 ];
-   
-    mensajeSaludo += ("Hoy es ") + fecha.toLocaleDateString() ;
 
-    mensajeSaludo += (" Tenés " + pendientes )+ " pendientes hoy. ";
+function saludoPrincipal (usuario, fecha, pendientes, evento, comprasArray) {
+ let mensajeSaludo = ("Bienvenido "+usuario);
 
-  //ver como hacer que el mensaje siga sumando cosas... 
-  
-  if (evento) {
-       mensajeSaludo = "Tenés un evento a la tarde ";
+
+   mensajeSaludo += ("Hoy es ") + fecha.toLocaleDateString() ;
+
+
+   mensajeSaludo += (" Hay " + pendientes )+ " pendientes. ";
+
+
+   if (evento) {
+       mensajeSaludo += "Tenés un evento a la tarde ";
    } else {
-       mensajeSaludo = "No tenés eventos importantes hoy ";
+       mensajeSaludo += "No tenés eventos importantes ";
    }
 
+   //array compras
+
+   if (comprasArray.includes("verdura")) {
+       mensajeSaludo += "y no te olvides de ir al super a comprar verdura";
+   } else {
+       mensajeSaludo += "y tus compras están al día";
+   }
+   
+   return mensajeSaludo;
 }
 
+//Llamo mi funcion principal:
+
+const mensajeFinal = saludoPrincipal(
+   nombreUsuario,
+  
+   fechaHoy,
+   totalPendientesHoy,
+   tieneEventoTarde,
+   listaDeCompras
+);
+
+
+alert(mensajeFinal);
 
 
 
